249e8b14b277639a152bd0f9551e5a04f72a5a0c
Marek <xbox360.rozek@gmail.com>
2022-05-23 13:15:35 +0200

    feat: init configuration

---
+++ main.tf
@@ -0,0 +1,102 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = local.azs_names
+  private_subnets = local.private_subnet_cidrs
+  public_subnets  = local.public_subnet_cidrs
+
+  enable_nat_gateway     = var.vpc_nat_setting.enable_nat_gateway
+  single_nat_gateway     = !var.vpc_nat_setting.multi_az_nat_gateway
+  one_nat_gateway_per_az = var.vpc_nat_setting.multi_az_nat_gateway
+  enable_dns_hostnames   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared",
+    "kubernetes.io/role/elb"                            = "1"
+    "subnet"                                            = "public"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"                   = "1"
+    "subnet"                                            = "private"
+  }
+  tags = local.tags
+}
+
+module "kubernetes_secrets_encryption_key" {
+  source      = "modules/encryption"
+  org         = var.org
+  environment = var.environment
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = var.eks_cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  create_cluster_security_group   = true
+  create_node_security_group      = true
+  cluster_enabled_log_types       = var.eks_cluster_log_types
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  cluster_encryption_config = [
+    {
+      provider_key_arn = module.kubernetes_secrets_encryption_key.kubernetes_secrets_encryption_key_arn
+      resources        = ["secrets"]
+    }
+  ]
+
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = var.eks_cluster_default_node_group
+  eks_managed_node_groups         = var.eks_cluster_additional_node_groups
+
+  # Fargate Profile(s)
+  fargate_profiles = {}
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  #  aws_auth_roles = [
+  #    {
+  #      rolearn  = "arn:aws:iam::66666666666:role/role1"
+  #      username = "role1"
+  #      groups   = ["system:masters"]
+  #    },
+  #  ]
+  #
+  #  aws_auth_users = [
+  #    {
+  #      userarn  = "arn:aws:iam::66666666666:user/user1"
+  #      username = "user1"
+  #      groups   = ["system:masters"]
+  #    },
+  #    {
+  #      userarn  = "arn:aws:iam::66666666666:user/user2"
+  #      username = "user2"
+  #      groups   = ["system:masters"]
+  #    },
+  #  ]
+
+
+  tags = local.tags
+}
---
+++ modules/encryption/main.tf
@@ -0,0 +1,9 @@
+resource "aws_kms_key" "kubernetes_secrets_encryption_key" {
+  description             = "${var.org}-${var.environment}-kubernetes-secret-emcyrption-key"
+  deletion_window_in_days = 10
+}
+
+resource "aws_kms_alias" "kubernetes_secrets_encryption_key" {
+  name                    = "alias/${var.org}-${var.environment}-kubernetes-secrets-enccryption-key"
+  target_key_id           = aws_kms_key.kubernetes_secrets_encryption_key.id
+}
