664397dade907b9bc36f9b86a15ed3e095729725
Adam <adam.pietrzykowski@softwaremill.pl>
2023-04-05 13:13:34 +0200

    Add KMS alias ARN output

--- outputs.tf
+++ outputs.tf
@@ -82,3 +82,8 @@ output "kubernetes_secrets_encryption_key_arn" {
   value       = module.kubernetes_secrets_encryption_key.kubernetes_secrets_encryption_key_arn
   description = "Encryption key ARN created for encryption of Kubernetes secrets."
 }
+
+output "kubernetes_secrets_encryption_alias_arn" {
+  value       = module.kubernetes_secrets_encryption_key.kubernetes_secrets_encryption_alias_arn
+  description = "Encryption KMS key alias ARN. Key is used for encryption of Kubernetes secrets."
+}
